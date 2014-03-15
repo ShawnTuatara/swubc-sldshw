0d4f573422339559253bb480a194766d6003d98f
Shawn Clark <shawn.tuatara@gmail.com>
2014-03-15 14:20:58 -0700

    Removed security
    Added static js example
    
    html templating

---
+++ src/main/java/com/startupweekend/ubc/sldshw/web/FrontendController.java
@@ -0,0 +1,12 @@
+package com.startupweekend.ubc.sldshw.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FrontendController {
+	@RequestMapping("/")
+	public String frontend() {
+		return "home";
+	}
+}
---
+++ src/main/java/com/startupweekend/ubc/sldshw/web/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.startupweekend.ubc.sldshw.web;
+
+import org.springframework.context.annotation.Configuration;
+
+//
+//@Configuration
+//@EnableWebMvcSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.authorizeRequests().antMatchers("/").permitAll();
+//	}
+//
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth)
+//			throws Exception {
+//		auth.inMemoryAuthentication().withUser("user").password("password")
+//				.roles("USER");
+//	}
+//}

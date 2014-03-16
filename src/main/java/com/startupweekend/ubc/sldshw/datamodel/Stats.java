f828bd9a6c9bec06700e49125f2e07af60f2a691
shu <shuzhang0@gmail.com>
2014-03-16 11:50:20 -0700

    enable free form voting; realtime anlytics

--- src/main/java/com/startupweekend/ubc/sldshw/datamodel/Stats.java
+++ src/main/java/com/startupweekend/ubc/sldshw/datamodel/Stats.java
@@ -1,9 +1,19 @@
 package com.startupweekend.ubc.sldshw.datamodel;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
+/**
+ * Collects stats on page annotations. Thread-safe.
+ * @author shu
+ *
+ */
 @ToString
 @EqualsAndHashCode
 public class Stats {
@@ -11,8 +21,14 @@ public class Stats {
 	@Getter
 	private final String pageId;
 	
-	private int heartCount;
-	private int questionCount;
+	@Getter
+	private AtomicInteger heartCount = new AtomicInteger(0);
+	@Getter
+	private AtomicInteger questionCount = new AtomicInteger(0);
+	
+	private Object lock = new Object();
+	@Getter
+	private Map<String, AtomicInteger> voteCounts = new HashMap<String, AtomicInteger>();
 	
 	public Stats(String pageId) {
 		this.pageId = pageId;
@@ -24,11 +40,22 @@ public class Stats {
 		}
 		
 		if (annotation.getHeart() != null && annotation.getHeart()) {
-			this.heartCount++;
+			this.heartCount.incrementAndGet();
 		}
 		
 		if (annotation.getQuestion() != null && annotation.getQuestion()) {
-			this.questionCount++;
+			this.questionCount.incrementAndGet();
+		}
+		
+		if (annotation.getVote() != null && !annotation.getVote().isEmpty()) {
+			if (!voteCounts.containsKey(annotation.getVote())) {
+				synchronized(lock) {
+					if (!voteCounts.containsKey(annotation.getVote())) {
+						voteCounts.put(annotation.getVote(), new AtomicInteger(0));
+					}
+				}
+			}
+			voteCounts.get(annotation.getVote()).incrementAndGet();
 		}
 	}
 }
--- src/main/java/com/startupweekend/ubc/sldshw/messaging/MessagingController.java
+++ src/main/java/com/startupweekend/ubc/sldshw/messaging/MessagingController.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -21,12 +22,17 @@ public class MessagingController {
 	
 	private final Object lock = new Object();
 	private final Object presentationPageLock = new Object();
+	private final Object presentationPageStatsLock = new Object();
+	private final Object presentationPageStatsLock2 = new Object();
 	
 	/** Maps presentation -> user -> pageId -> annotations. */
 	private Map<String, Map<String, Map<String, PageAnnotation>>> data
 		= new HashMap<String, Map<String, Map<String, PageAnnotation>>>();
 	private Map<String, String> userSessionMappingHack = new HashMap<String, String>();
 	private Map<String, String> presentationPage = new HashMap<String, String>();
+	
+	/** Maps presentation -> pageId -> summary stats */
+	private Map<String, Map<String, Stats>> presentationPageStats = new HashMap<String, Map<String, Stats>>();
 
 	@MessageMapping("/presentation/{id}/page")
 	public String updatePageId(
@@ -38,20 +44,41 @@ public class MessagingController {
 		}
 		return pageId;
 	}
-	
+
 	@SubscribeMapping("/topic/presentation/{id}/page")
 	public String subscribeToUpdatePage(@DestinationVariable("id") String presentationId) {
 		System.out.println("subscribeToUpdatePage - presentationId: " + presentationId);
 		return presentationPage.get(presentationId);
 	}
 	
-	@MessageMapping("/presentation/{id}")
-	public void annotatePage(
+	@MessageMapping("/presentation/{id}/page/{pageId}")
+	public Stats annotatePage(
 			@DestinationVariable("id") String presentationId,
 			@Header(SimpMessageHeaderAccessor.SESSION_ID_HEADER) String userId,
 			PageAnnotation annotation) {
 		System.out.println("annotatePage - presentationId: " + presentationId + ", userId: " + userId + ", annotation: " + annotation);
 		
+		/* update stats tracking for this page */
+		if (!presentationPageStats.containsKey(presentationId)) {
+			synchronized(presentationPageStatsLock) {
+				if (!presentationPageStats.containsKey(presentationId)) {
+					presentationPageStats.put(presentationId, new HashMap<String, Stats>());
+				}
+			}
+		}
+		Map<String, Stats> pageStats = presentationPageStats.get(presentationId);
+		
+		if (!pageStats.containsKey(annotation.getPageId())) {
+			synchronized(presentationPageStatsLock2) {
+				if (!pageStats.containsKey(annotation.getPageId())) {
+					pageStats.put(annotation.getPageId(), new Stats(annotation.getPageId()));
+				}
+			}
+		}
+		Stats summary = pageStats.get(annotation.getPageId());
+		summary.collect(annotation);
+		
+		/* update fine grained data */
 		// get the per presentation data set
 		if (!data.containsKey(presentationId)) {
 			synchronized(lock) {
@@ -76,7 +103,7 @@ public class MessagingController {
 		// look up existing annotation and 'update' it with new info only
 		if (!existingAnnotations.containsKey(annotation.getPageId())) {
 			existingAnnotations.put(annotation.getPageId(), annotation);
-			return;
+			return summary;
 		}
 		
 		PageAnnotation existingAnnotation = existingAnnotations.get(annotation.getPageId());
@@ -89,7 +116,12 @@ public class MessagingController {
 		if (annotation.getComments() != null) {
 			existingAnnotation.getComments().addAll(annotation.getComments());
 		}
+		if (annotation.getVote() != null && !annotation.getVote().isEmpty()) {
+			existingAnnotation.setVote(annotation.getVote());
+		}
+		
 		System.out.println("updated annotations: " + existingAnnotation);
+		return summary;
 	}
 	
 	@MessageMapping("/presentation/{id}/summary")
@@ -105,7 +137,6 @@ public class MessagingController {
 		}
 
 		List<PageAnnotation> userAnnotations = new ArrayList<PageAnnotation>();
-		Map<String, Stats> presentationStats = new HashMap<String, Stats>();
 		
 		Map<String, Map<String, PageAnnotation>> presentationData = data.get(presentationId);
 		if (presentationData == null) {
@@ -117,17 +148,12 @@ public class MessagingController {
 			userAnnotations.addAll(savedAnnotations.values());
 		}
 		
-		for (Map<String, PageAnnotation> annotationsForUser : presentationData.values()) {
-			for (PageAnnotation annotation: annotationsForUser.values()) {
-				if (!presentationStats.containsKey(annotation.getPageId())) {
-					presentationStats.put(annotation.getPageId(), new Stats(annotation.getPageId()));
-				}
-				presentationStats.get(annotation.getPageId()).collect(annotation);
-			}
+		Map<String, Stats> pageStats = presentationPageStats.get(presentationId);
+		List<Stats> stats = null;
+		if (pageStats != null) {
+			stats = new ArrayList<Stats>(pageStats.size());
+			stats.addAll(pageStats.values());
 		}
-		
-		List<Stats> stats = new ArrayList<Stats>(presentationStats.size());
-		stats.addAll(presentationStats.values());
 		Response response = new Response(userAnnotations, stats);
 		System.out.println("result: " + response);
 		return response;

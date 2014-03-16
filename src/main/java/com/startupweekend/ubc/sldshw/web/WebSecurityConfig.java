f3d66480144cf7a9c7f31029281dd984dead53d7
Shawn Clark <shawn.tuatara@gmail.com>
2014-03-15 17:39:42 -0700

    Adding template mappings
    Spring security with thymeleaf sec namespace

--- src/main/java/com/startupweekend/ubc/sldshw/web/WebSecurityConfig.java
+++ src/main/java/com/startupweekend/ubc/sldshw/web/WebSecurityConfig.java
@@ -1,20 +1,26 @@
 package com.startupweekend.ubc.sldshw.web;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
-//
-//@Configuration
-//@EnableWebMvcSecurity
-//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http.authorizeRequests().antMatchers("/").permitAll();
-//	}
-//
-//	@Override
-//	protected void configure(AuthenticationManagerBuilder auth)
-//			throws Exception {
-//		auth.inMemoryAuthentication().withUser("user").password("password")
-//				.roles("USER");
-//	}
-//}
+@Configuration
+@EnableWebMvcSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/user").authenticated()
+				.anyRequest().permitAll();
+		http.formLogin().loginPage("/login").permitAll().and().logout()
+				.permitAll();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth)
+			throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("password")
+				.roles("USER");
+	}
+}
